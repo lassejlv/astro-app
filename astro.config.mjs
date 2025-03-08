4f88101392e6c7306b9b7b15a7100609f11b040a
lasse <77295879+lassejlv@users.noreply.github.com>
2025-03-08 23:24:27 +0100

    Inital commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+
+import tailwindcss from '@tailwindcss/vite'
+import node from '@astrojs/node'
+import react from '@astrojs/react'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  output: 'server',
+
+  adapter: node({
+    mode: 'standalone',
+  }),
+
+  integrations: [react()],
+})
